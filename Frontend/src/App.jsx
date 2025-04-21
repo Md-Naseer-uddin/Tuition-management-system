1e5e171d4f8c920e681a03b732f5d257ce378685
Md Naseer uddin <umdnaseer17@gmail.com>
2025-04-21 20:44:49 +0530

    admin

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
 import './App.css'
 import Dashboard from './admin/Dashboard'
 import Centers from './admin/Centers'
+import Tutor from './admin/Tutors'
+import Students from './admin/Students'
+import Attendance from './admin/Attendance'
 
 function App() {
-  
+
   return (
     <>
       <Dashboard/>
-      {/* <Centers/> */}
+      <Centers/>
+      <Tutor/>
+      <Students/>
+      <Attendance/>
     </>
   )
 }
-
 export default App
--- Frontend/src/admin/Centers.jsx
+++ Frontend/src/admin/Centers.jsx
@@ -1,41 +1,58 @@
 import Sidebar from "./Sidebar";
 
 const Centers = () => {
-    return (
-        <>
-            <div className="flex">
-                <Sidebar />
-                <div className="">
-                    <h1 className="text-3xl font-normal mt-4 ml-2 mb-3">Centers Management</h1>
-                    <div className="flex justify-between">
-                    <div>
-                        Total Centers
-                        <br />
-                        x
-                    </div>
-                    <div>Total Tutors
-                        <br />
-                        y
-                    </div>
-                    <div>Total Students
-                        <br />
-                        z
-                    </div>
-                    </div>
-                    <div className="flex justify-around">
-                        <input className="border rounded mt-4 p-1 text-[15px] w-[300px]"
-                         type="text" placeholder="Search Centers"/>
-                        <div className="flex gap-4">
-                        <button className="text-[25px] p-3 h-13 m-2 border rounded">Add Center</button>
-                        <button className="text-[25px] p-3 h-13 m-2 border rounded">Remove Center</button>
-                        </div>
-                    </div>
-                    
-                </div>
-
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="w-[80%]">
+        <div className="w-full">
+          <h1 className="text-3xl font-normal mt-4 ml-2 mb-3">
+            Centers Management
+          </h1>
+          <div className="flex justify-between">
+            <div>
+              Total Centers
+              <br />x
             </div>
-        </>
-    )
-}
+            <div>
+              Total Tutors
+              <br />y
+            </div>
+            <div className="mr-3">
+              Total Students
+              <br />z
+            </div>
+          </div>
+          <div className="flex justify-around">
+            {/* Search bar */}
+            <div className=" w-[100%] flex justify-between items-start p-4 ">
+              {/* Input on the left */}
+              <input
+                className="border rounded p-2 text-[15px] w-[300px]"
+                type="text"
+                placeholder="Search Centers"
+              />
 
-export default Centers
+              {/* Buttons on the right */}
+              <div className="flex gap-4">
+                <button className="text-[16px] px-4 py-2 border rounded">
+                  Add Center
+                </button>
+                <button className="text-[16px] px-4 py-2 border rounded">
+                  Remove Center
+                </button>
+              </div>
+            </div>
+          </div>
+          <h1 className="text-2xl mt-4 ml-4 mb-2 font-normal">Centers List</h1>
+          <div className="flex flex-col gap-4">
+            <div className="w-100 h-30">Center1</div>
+            <div className="w-100 h-30">Center2</div>
+            <div className="w-100 h-30">Center3</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Centers;
